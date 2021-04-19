de6d8073ef85413a5a2a684d0e4200ec11d8d03d
tdallstr@gmail.com <123654Qw/>
2021-04-19 12:14:59 +0300

    m

--- 4.DFX_BUSD.py
+++ 4.DFX_BUSD.py
@@ -35,14 +35,23 @@ driver.switch_to.window(current_window)
 
 add_liq_Defirex = driver.find_element_by_link_text('Add Liquidity using DeFireX').click()
 max = driver.find_element_by_css_selector('.mat-form-field-suffix.ng-star-inserted').click()
-Get_LP_tokens = driver.find_element_by_css_selector('.description>.custom_button.green').click()
-modal_Ok = driver.find_element_by_css_selector('.mat-focus-indicator.mat-raised-button.mat-button-base.mat-primary').click()
-close = driver.find_element_by_css_selector('.mat-focus-indicator.mat-button.mat-button-base').click()
+input1 = driver.find_element_by_id('mat-input-12')
+input1.click()
+input1.send_keys(123)
+input2 = driver.find_element_by_id('mat-input-13')
+input2.click()
+input2.send_keys(123)
+message1 = driver.find_element_by_css_selector('.input-group>.description>:nth-child(1)').text
+assert message1 == 'Insufficient BUSD balance'
+message2 = driver.find_element_by_css_selector('.input-group>.description>:nth-child(2)').text
+assert message2 == 'Insufficient DFX balance'
+
+close = driver.find_element_by_css_selector('.mat-dialog-actions>:nth-child(1)>:nth-child(1)').click()
 
 info = driver.find_element_by_css_selector('.header>.lp>:nth-child(2)>:nth-child(1)>:nth-child(1)').click()
-time.sleep(2)
+time.sleep(1)
 info_ok = driver.find_element_by_css_selector('.mat-focus-indicator.mat-raised-button.mat-button-base.mat-primary').click()
-
+DFX_BUSD = driver.find_element_by_css_selector('.pools.curr1.ng-star-inserted>:nth-child(1)').click()
 withdraw = driver.find_element_by_css_selector('.dep>.mat-tab-group.mat-primary>.mat-tab-header>.mat-tab-label-container>.mat-tab-list>.mat-tab-labels>:nth-child(2)').click()
 MAX_withdraw = driver.find_element_by_link_text('MAX withdraw and Get DFX-BUSD-LP tokens').click()
 Change_DFX_BUSD = driver.find_element_by_link_text('Change DFX-BUSD-LP token on PancakeSwap -> get DFX and BUSD').click()
--- 5.pools.py
+++ 5.pools.py
@@ -7,23 +7,19 @@ driver = webdriver.Chrome()
 driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
 #pools = .pools.curr1.ng-star-inserted
-pool2 = driver.find_element_by_css_selector('.pools.curr1.ng-star-inserted>:nth-child(2)').click()
+pool = driver.find_element_by_css_selector('.pools.curr1.ng-star-inserted>:nth-child(2)').click()
 input2 = driver.find_element_by_css_selector('.active>.moreInfo>.dep>.mat-tab-group>.mat-tab-body-wrapper>.mat-tab-body-active>:nth-child(1)>:nth-child(2)>.input>:nth-child(1)')
 input2.send_keys('123')
 
-#pool3 = driver.find_element_by_css_selector('.pools.curr1.ng-star-inserted>:nth-child(3)').click()
-#input3 = driver.find_element_by_css_selector('.active>.moreInfo>.dep>.mat-tab-group>.mat-tab-body-wrapper>.mat-tab-body-active>:nth-child(1)>:nth-child(2)>.input>:nth-child(1)')
-#input3.send_keys('123')
-
-#pool3 = driver.find_element_by_css_selector('.pools.curr1.ng-star-inserted>:nth-child(4)').click()
-#input3 = driver.find_element_by_css_selector('.active>.moreInfo>.dep>.mat-tab-group>.mat-tab-body-wrapper>.mat-tab-body-active>:nth-child(1)>:nth-child(2)>.input>:nth-child(1)')
-#input3.send_keys('123')
-
-pool3 = driver.find_element_by_css_selector('.pools.curr1.ng-star-inserted>:nth-child(5)').click()
+pool1 = driver.find_element_by_css_selector('.pools.curr1.ng-star-inserted>:nth-child(3)').click()
 input3 = driver.find_element_by_css_selector('.active>.moreInfo>.dep>.mat-tab-group>.mat-tab-body-wrapper>.mat-tab-body-active>:nth-child(1)>:nth-child(2)>.input>:nth-child(1)')
 input3.send_keys('123')
 
-pool3 = driver.find_element_by_css_selector('.pools.curr1.ng-star-inserted>:nth-child(6)').click()
+pool2 = driver.find_element_by_css_selector('.pools.curr1.ng-star-inserted>:nth-child(4)').click()
+input3 = driver.find_element_by_css_selector('.active>.moreInfo>.dep>.mat-tab-group>.mat-tab-body-wrapper>.mat-tab-body-active>:nth-child(1)>:nth-child(2)>.input>:nth-child(1)')
+input3.send_keys('123')
+
+pool3 = driver.find_element_by_css_selector('.pools.curr1.ng-star-inserted>:nth-child(5)').click()
 input3 = driver.find_element_by_css_selector('.active>.moreInfo>.dep>.mat-tab-group>.mat-tab-body-wrapper>.mat-tab-body-active>:nth-child(1)>:nth-child(2)>.input>:nth-child(1)')
 input3.send_keys('123')
 
