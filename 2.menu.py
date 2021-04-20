15ded1cd95830c161ae409341f3be9b2f6deb9dd
tdallstr@gmail.com <123654Qw/>
2021-04-20 15:53:45 +0300

    2

--- 2.menu.py
+++ 2.menu.py
@@ -10,11 +10,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-#pools = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".menu>#pools")))
-
-
-DFX_BUSD = driver.find_element_by_css_selector('.header>.lp').click()
-
 wiki = driver.find_element_by_link_text('Wiki').click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
