69153b921619ad09c1d9c11db8655dd6abbf6382
tdallstr@gmail.com <123654Qw/>
2021-04-20 16:07:49 +0300

    2

--- 2.menu.py
+++ 2.menu.py
@@ -5,6 +5,12 @@ driver.get('https://defirex.org/')
 driver.maximize_window()
 driver.implicitly_wait(5)
 
+DFX_Airdrop = driver.find_element_by_link_text('DFX Airdrop').click()
+current_window = driver.current_window_handle
+assert driver.current_url == "https://defirex.org/airdrop-dfx"
+driver.close()
+driver.switch_to.window(current_window)
+
 pscr = driver.find_element_by_id('pools')
 pscr.click()
 
@@ -19,6 +25,8 @@ driver.switch_to.window(current_window)
 DFX_Airdrop = driver.find_element_by_link_text('DFX Airdrop').click()
 current_window = driver.current_window_handle
 assert driver.current_url == "https://defirex.org/airdrop-dfx"
+driver.close()
+driver.switch_to.window(current_window)
 
 buy_DFX = driver.find_element_by_link_text('Buy DFX').click()
 
