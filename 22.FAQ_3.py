13c7af8fcde9d0618e738a7a6b9b9f0a3ceac9ab
tdallstr@gmail.com <123654Qw/>
2021-04-20 12:30:27 +0300

    22

--- 22.FAQ_3.py
+++ 22.FAQ_3.py
@@ -1,14 +1,21 @@
 import time
 
 from selenium import webdriver  # импортируем webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
+driver.maximize_window()
 driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
 
 How_the_defirex = driver.find_element_by_id('mat-expansion-panel-header-2').click()
 
-medium = driver.find_element_by_id('article_medium').click()
+medium = WebDriverWait(driver, 20).until(		# говорим Selenium проверять в течение 20 секунд, пока кнопка не станет кликабельной
+        EC.element_to_be_clickable((By.ID, "article_medium")))
+
+medium.click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
 driver.switch_to.window(new_window)
