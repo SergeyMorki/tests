d61d27841b1714c0cf427be906fd5112e2ecf4a9
tdallstr@gmail.com <123654Qw/>
2021-04-20 13:27:25 +0300

    23

--- 23.FAQ_4.py
+++ 23.FAQ_4.py
@@ -7,8 +7,12 @@ driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
 
 How_profit = driver.find_element_by_id('mat-expansion-panel-header-3').click()
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-balance_on_compound = driver.find_element_by_id('Balance_on_Compound').click()
+balance_on_compound = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "Balance_on_Compound")))
+balance_on_compound.click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
 driver.switch_to.window(new_window)
