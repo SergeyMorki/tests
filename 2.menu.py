da5cb8778da5dee39fefa829c674e77df842a4bf
tdallstr@gmail.com <123654Qw/>
2021-04-20 17:16:00 +0300

    2

--- 2.menu.py
+++ 2.menu.py
@@ -1,14 +1,15 @@
 from selenium import webdriver  # импортируем webdriver
 
 driver = webdriver.Chrome()
-driver.maximize_window()
+
 driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
 
-pscr = driver.find_element_by_id('pools')
+menu = driver.find_element_by_class_name('mobile_button').click()
+pscr = driver.find_element_by_link_text('Pools')
 pscr.click()
 
-wiki = driver.find_element_by_id('Wiki').click()
+wiki = driver.find_element_by_link_text('Wiki').click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
 driver.switch_to.window(new_window)
@@ -16,11 +17,9 @@ assert driver.current_url == "https://wiki.defirex.org/"
 driver.close()
 driver.switch_to.window(current_window)
 
-DFX_Airdrop = driver.find_element_by_id('airDrop_dfx').click()
+DFX_Airdrop = driver.find_element_by_link_text('DFX Airdrop').click()
 current_window = driver.current_window_handle
 assert driver.current_url == "https://defirex.org/airdrop-dfx"
-driver.close()
-driver.switch_to.window(current_window)
 
 buy_DFX = driver.find_element_by_link_text('Buy DFX').click()
 driver.close()
