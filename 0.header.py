8d0728c990acc0a0e1ac3a03836c29716c23fd12
tdallstr@gmail.com <123654Qw/>
2021-04-26 17:36:52 +0300

    0

--- 0.header.py
+++ 0.header.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get('https://defirex.org/')
