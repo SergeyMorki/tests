78721780482670bc3bb4efb82ec95d29723429ef
tdallstr@gmail.com <123654Qw/>
2021-05-17 15:13:50 +0300

    0

--- 0.header.py
+++ 0.header.py
@@ -25,71 +25,7 @@ driver.switch_to.window(current_window)
 
 users_balance.click()
 Users_balances_on_BSC = driver.find_element_by_link_text('Users balances on BSC').click()
-DAI_funds_holders = driver.find_element_by_link_text('DAI funds holders').click()
-new_window = driver.window_handles[1]
-current_window = driver.current_window_handle
-driver.switch_to.window(new_window)
-assert driver.current_url == "https://www.bscscan.com/token/0x308853AeC7cF0ECF133ed19C0c1fb3b35f5a4E7B"
-driver.close()
-driver.switch_to.window(current_window)
 
-users_balance.click()
-Users_balances_on_BSC = driver.find_element_by_link_text('Users balances on BSC').click()
-BUSD_funds_holders = driver.find_element_by_link_text('BUSD funds holders').click()
-new_window = driver.window_handles[1]
-current_window = driver.current_window_handle
-driver.switch_to.window(new_window)
-assert driver.current_url == "https://www.bscscan.com/token/0x987f04DecE1c5AE9E69cF4F93D00bBE2cA5Af98c"
-driver.close()
-driver.switch_to.window(current_window)
-
-users_balance.click()
-Users_balances_on_BSC = driver.find_element_by_link_text('Users balances on BSC').click()
-DFXBUSD_funds_holders = driver.find_element_by_link_text('DFXBUSD funds holders').click()
-new_window = driver.window_handles[1]
-current_window = driver.current_window_handle
-driver.switch_to.window(new_window)
-assert driver.current_url == "https://www.bscscan.com/token/0xe4743bee99d515a2C36C30B37e3756750fE24c9D"
-driver.close()
-driver.switch_to.window(current_window)
-
-users_balance.click()
-Users_balances_on_BSC = driver.find_element_by_link_text('Users balances on BSC').click()
-DFX_funds_holders = driver.find_element_by_link_text('DFX funds holders').click()
-new_window = driver.window_handles[1]
-current_window = driver.current_window_handle
-driver.switch_to.window(new_window)
-assert driver.current_url == "https://www.bscscan.com/token/0x11340dC94E32310FA07CF9ae4cd8924c3cD483fe"
-driver.close()
-driver.switch_to.window(current_window)
-
-users_balance.click()
-Users_balances_on_BSC = driver.find_element_by_link_text('Users balances on BSC').click()
-BTCB_funds_holders = driver.find_element_by_link_text('BTCB funds holders').click()
-new_window = driver.window_handles[1]
-current_window = driver.current_window_handle
-driver.switch_to.window(new_window)
-assert driver.current_url == "https://www.bscscan.com/token/0x7CA1fEA7d198cEaE9A319B5EE89E860aAB7D82d7"
-driver.close()
-driver.switch_to.window(current_window)
-
-users_balance.click()
-Users_balances_on_BSC = driver.find_element_by_link_text('Users balances on BSC').click()
-BTCB2_funds_holders = driver.find_element_by_link_text('BUSD2 funds holders').click()
-new_window = driver.window_handles[1]
-current_window = driver.current_window_handle
-driver.switch_to.window(new_window)
-assert driver.current_url == "https://www.bscscan.com/token/0xAB2f29783265940305EA99573AA18bD301911a09"
-driver.close()
-driver.switch_to.window(current_window)
-
-users_balance.click()
-Users_balances_on_BSC = driver.find_element_by_link_text('Users balances on BSC').click()
-BTCB2_funds_holders = driver.find_element_by_link_text('BUSD+USDT+USDC funds holders').click()
-new_window = driver.window_handles[1]
-current_window = driver.current_window_handle
-driver.switch_to.window(new_window)
-assert driver.current_url == "https://www.bscscan.com/token/0x5DaA08aF18104702d4a387027E09b9b83b0fc720"
 driver.close()
 driver.quit()
 
